4fc4e803d612a31b9f53c083b4245aa9989baa53
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-18 17:18:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,29 +2,27 @@
 katz_deli = []
 
 #deli begins as an empty array, currently no customers.
-#check if array is empty and print
+#check if array is empty and output
 #if array isn't empty iterate through each index and output the info to the deli line
 
 def line(katz_deli)
  if katz_deli.empty?
    puts "The line is currently empty."
  else current_line = "The line is currently:"
-   katz_deli.each.with_index(1) do |person, i|
-     current_line << " #{i}. #{person}"
+   katz_deli.each.with_index(1) do |cust, i|
+     current_line << " #{i}. #{cust}"
    end
    puts current_line
  end
 end 
 
-#have to add an element to the end of the current array with shovel
-#create a string for the customer
-#interpolate the customers name and their position in line
-#How do i check the customers place in line?
-#shoveling adds to end of array, array.length
-
-def take_a_number(katz_deli, new_customer)
-  katz_deli << new_customer
-  puts "Welcome, #{new_customer}. You are number #{katz_deli.length} in line."
+#create a variable for the line including elements of position and name
+#iterate over the array and grab its index starting with 1
+#interpolate customers name and their position in line
+#shovel to end of array
+ cust_number = 0
+def take_a_number
+  cust_number += 1
 end
 
 #checking again to see if the array is empty
